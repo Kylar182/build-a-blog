32c246498ed0024f08e72f53798fb7e776571c15
Kylar182 <ajbeeman@kfsoftwaresolutions.com>
2018-09-04 21:11:24 -0500

    No User Build A Blog

--- main.py
+++ main.py
@@ -11,35 +11,42 @@ app.secret_key = '3j2kl3j4kl123j415@@28n'
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
+    #name = db.Column(db.String(120))
     title = db.Column(db.String(120))
     body = db.Column(db.String(1200))
 
-    def __init__(self, name, title, body):
-        self.name = name
+    def __init__(self, title, body):
+        #self.name = name
         self.title = title
         self.body = body
 
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    return redirect("/blog")
+
 @app.route('/blog', methods=['POST', 'GET'])
 def index():
-    if request.method == 'POST':
-        task_name = request.form['task']
-        new_task = Task(task_name, current_user)
-        db.session.add(new_task)
-        db.session.commit()
+    if request.args.get('id'):
+        blog_posts = Blog.query.filter_by('id').all()
+        title_h1 = blog_posts[0].title
+        body = blog_posts[0].body
+    else:
+        blog_posts = Blog.query.filter_by().all()
+        title_h1 = "Build a Blog"
+        body = ''
 
-    blog_posts = Blog.query.filter_by().all()
-    return render_template('blog.html',title="Build a Blog", blog_posts=blog_posts)
+    return render_template('blog.html', title_h1=title_h1, blog_posts=blog_posts, body=body)
 
 @app.route('/newpost', methods=['POST', 'GET'])
-def index():
+def new_post():
     if request.method == 'POST':
-        task_name = request.form['task']
-        new_task = Task(task_name, current_user)
-        db.session.add(new_task)
+        new_title = request.form['title']
+        new_body = request.form['body']
+        new_blog = Blog(new_title, new_body)
+        db.session.add(new_blog)
         db.session.commit()
 
-    return render_template('newpost.html',title="Add a Blog Entry")
+    return render_template('newpost.html')
 
 
 if __name__ == '__main__':
